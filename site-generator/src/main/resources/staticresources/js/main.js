d8bebe632218785ea188ef1b2cc6e834efecf34c
James R. Perkins <jperkins@redhat.com>
2024-06-03 14:38:27 -0700

    Upgrade bootstrap to 5.3 and removed JQuery support as it's no longer required.
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- site-generator/src/main/resources/staticresources/js/main.js
+++ site-generator/src/main/resources/staticresources/js/main.js
@@ -1,15 +1,17 @@
-$(function(){
+(() => {
+   "use strict";
 
-    $("[data-toggle=popover-provider-points]")
-      .on('click', function(e){
-            e.preventDefault();
-       })
-       .popover({
-          html : true,
-          content: function() {
-            return $(this.attributes['data-target'].value).html();
-          }
+   window.addEventListener("load", () => {
+      // Allow HTML data in popover's
+      const popovers = document.querySelectorAll("[data-bs-toggle='popover']");
+      popovers.forEach((e) => {
+         let opts = {};
+         if (e.hasAttribute("data-bs-target")) {
+            opts.html = true;
+            opts.content = document.querySelector(e.getAttribute("data-bs-target")).innerHTML;
+         }
+         new bootstrap.Popover(e, opts);
       });
-
-});
+   });
+})()
 
